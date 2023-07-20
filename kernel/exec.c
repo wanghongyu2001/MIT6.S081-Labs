12ebc78dcbf9ae6b5e566288e75186a9dd01e63c
wanghongyu2001 <1354998814@qq.com>
2023-07-20 14:16:11 +0600

    print page_table

--- kernel/exec.c
+++ kernel/exec.c
@@ -8,6 +8,7 @@
 #include "elf.h"
 
 static int loadseg(pde_t *pgdir, uint64 addr, struct inode *ip, uint offset, uint sz);
+void vmprint(pagetable_t pagetable, uint64 depth);
 
 int
 exec(char *path, char **argv)
@@ -115,7 +116,7 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
-
+  if (p->pid == 1) vmprint(p->pagetable, 0);
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
@@ -151,4 +152,25 @@ loadseg(pagetable_t pagetable, uint64 va, struct inode *ip, uint offset, uint sz
   }
   
   return 0;
+}
+
+void vmprint(pagetable_t pagetable, uint64 depth)
+{
+  
+  if (depth > 2)
+    return;
+  char* buf[] = { "..", ".. ..", ".. .. .." };
+  
+  for (int i = 0; i < 512; i++) {
+    if (!depth && !i)
+    {
+      printf("page table %p\n", pagetable);
+    }
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V)) {
+      uint64 child = PTE2PA(pte);
+      printf("%s%d: pte %p pa %p\n", buf[depth], i, pte, PTE2PA(pte));
+      vmprint((pagetable_t)child, depth + 1);
+    }
+  }
 }
